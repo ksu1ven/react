a190eb9432bc473cbc5054cea4d7f17971ea927b
Oksana <ksu1ven@mail.ru>
2023-11-08 19:06:09 +0300

    init: setup vitest

---
+++ src/tests/cardlist.test.tsx
@@ -0,0 +1,8 @@
+// import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+describe('App', () => {
+  it('ergt', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
