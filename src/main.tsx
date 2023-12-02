c272f49968f20da65a7703098e87e9d1b14306f9
Oksana <ksu1ven@mail.ru>
2023-12-03 00:46:59 +0300

    feat: add persist library

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/index.css';
-import { store } from './redux/store/store.ts';
+import { store, persistor } from './redux/store/store.ts';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import ErrorBoundary from './components/ErrorBoundary.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <PersistGate loading={null} persistor={persistor}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,19 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import formSlice from '../features/formSlice';
 import errorSlice from '../features/errorSlice';
 import countriesSlice from '../features/countriesSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const rootReducer = combineReducers({
+  form: formSlice,
+  error: errorSlice,
+  countries: countriesSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    form: formSlice,
-    error: errorSlice,
-    countries: countriesSlice,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
